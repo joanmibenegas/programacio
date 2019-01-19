9bc6f78a37c7e1503cad4ae43b0eb4b331312530
Joanmi <joanmiblack@gmail.com>
2019-01-19 18:23:13 +0100

    texteditor

---
+++ TextEditor/src/texteditor/TPEditor.java
@@ -0,0 +1,66 @@
+
+package texteditor;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.undo.UndoManager;import javax.swing.event.CaretListener;
+
+
+public class TPEditor {
+    //Instancies finestra
+    private JFrame finestraPrincipal;
+    private JMenuBar barraMenu;
+    private JToolBar barraEines;
+    private JTextArea areaText;
+    private JPopupMenu menuEmergent;
+    private JPanel barraEstat;
+    
+    //Instancies menu accessibles
+    private JCheckBoxMenuItem lineWrap;
+    private JCheckBoxMenuItem mostrarBarraEines;
+    private JCheckBoxMenuItem fixarBarraEines;
+    private JCheckBoxMenuItem mostrarBarraEstat;
+    
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
