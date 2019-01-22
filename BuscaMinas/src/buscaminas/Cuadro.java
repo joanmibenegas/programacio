dacecf81fe0d2a5488555d00c0f2ae344b445f85
Joanmi <joanmiblack@gmail.com>
2019-01-22 10:42:05 +0100

    BuscaMinas v2

---
+++ BuscaMinas/src/buscaminas/Cuadro.java
@@ -0,0 +1,22 @@
+package buscaminas;
+
+
+import javax.swing.JButton;
+
+public class Cuadro extends JButton {
+
+    private boolean mina = false; //Sera true si conté una mina
+
+    public Cuadro() {
+        super();
+        double random = Math.random();
+        if (random > 0.75) {
+            mina = true;
+        }
+    }
+
+    public boolean estatMinat() {
+        return mina;
+    }
+
+}
